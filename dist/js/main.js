18c8e710b57fd3790516ca547844513d9e56c80a
CLYI <ciaran.lyne@capco.com>
2022-11-24 11:45:41 +0000

    BSL Numbers working for two hands (fingerpose modified)

--- dist/js/main.js
+++ dist/js/main.js
@@ -11,25 +11,41 @@ async function main() {
 
     const GE = new fp.GestureEstimator(knownGestures);
 
-    const model = await handpose.load();
-    console.log("Handpose model loaded");
+    const handpose = handPoseDetection.SupportedModels.MediaPipeHands;
+    const hpConfig = {
+        runtime: 'tfjs',
+        modelType: 'full'
+    }
+
+    const model = await handPoseDetection.createDetector(handpose, hpConfig);
 
     const estimateHands = async () => {
 
         const predictions = await model.estimateHands(videoElement, true);
 
+        let signsFound = [];
+
         for(let i = 0; i < predictions.length; i++) {
-            const est = GE.estimate(predictions[i].landmarks, 7);
+
+            const est = GE.estimate(predictions[i].keypoints3D, 6);
 
             if(est.gestures.length > 0) {
                 let result = est.gestures.reduce((p, c) => { 
                     return (p.score > c.score) ? p : c;
                 });
 
-                resultElement.innerText = result.name;
-                debugElement.innerText = JSON.stringify(est.gestures);
-                
+                signsFound.push(result.name);
             }
+        }
+
+        resultElement.innerText = signsFound;
+
+        if(signsFound.length > 1) {
+            resultElement.innerText = signsFound;
+        } else if (signsFound.length == 1) {
+            resultElement.innerText = signsFound;
+        } else {
+            resultElement.innerText = " ";
         }
 
         setTimeout(() => { estimateHands(); }, 1000 / config.video.fps);
@@ -67,7 +83,6 @@ window.addEventListener("DOMContentLoaded", () => {
     ).then(video => {
         video.play();
         video.addEventListener("loadeddata", event => {
-        console.log("Camera is ready");
         main();
     })});
 });
--- dist/js/signs.js
+++ dist/js/signs.js
@@ -124,110 +124,11 @@ SIGN_5.addDirection(fp.Finger.Pinky, fp.FingerDirection.DiagonalUpLeft, 1.0);
 SIGN_5.addDirection(fp.Finger.Pinky, fp.FingerDirection.DiagonalUpRight, 1.0);
 
 
-//6
-const SIGN_6 = new fp.GestureDescription('6');
-
-SIGN_6.addCurl(fp.Finger.Thumb, fp.FingerCurl.HalfCurl, 1.0);
-SIGN_6.addCurl(fp.Finger.Thumb, fp.FingerCurl.NoCurl, 1.0);
-SIGN_6.addDirection(fp.Finger.Thumb, fp.FingerDirection.VerticalUp, 1.0);
-SIGN_6.addDirection(fp.Finger.Thumb, fp.FingerDirection.DiagonalUpLeft, 1.0);
-SIGN_6.addDirection(fp.Finger.Thumb, fp.FingerDirection.DiagonalUpRight, 1.0);
-
-SIGN_6.addCurl(fp.Finger.Index, fp.FingerCurl.FullCurl, 1.3);
-SIGN_6.addCurl(fp.Finger.Index, fp.FingerCurl.HalfCurl, 0.9);
-
-SIGN_6.addCurl(fp.Finger.Middle, fp.FingerCurl.FullCurl, 1.0);
-SIGN_6.addCurl(fp.Finger.Middle, fp.FingerCurl.HalfCurl, 0.9);
-
-SIGN_6.addCurl(fp.Finger.Ring, fp.FingerCurl.FullCurl, 1.0);
-SIGN_6.addCurl(fp.Finger.Ring, fp.FingerCurl.HalfCurl, 0.9);
-
-SIGN_6.addCurl(fp.Finger.Pinky, fp.FingerCurl.FullCurl, 1.0);
-SIGN_6.addCurl(fp.Finger.Pinky, fp.FingerCurl.HalfCurl, 0.9);
-
-
-//7
-const SIGN_7 = new fp.GestureDescription('7');
-
-SIGN_7.addCurl(fp.Finger.Thumb, fp.FingerCurl.NoCurl, 1.4);
-SIGN_7.addDirection(fp.Finger.Thumb, fp.FingerDirection.HorizontalLeft, 1.0);
-SIGN_7.addDirection(fp.Finger.Thumb, fp.FingerDirection.HorizontalRight, 1.0);
-
-SIGN_7.addCurl(fp.Finger.Index, fp.FingerCurl.NoCurl, 1.0);
-SIGN_7.addDirection(fp.Finger.Index, fp.FingerDirection.VerticalUp, 1.0);
-SIGN_7.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpLeft, 1.0);
-SIGN_7.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpRight, 1.0);
-
-SIGN_7.addCurl(fp.Finger.Middle, fp.FingerCurl.FullCurl, 1.0);
-SIGN_7.addCurl(fp.Finger.Middle, fp.FingerCurl.HalfCurl, 0.9);
-
-SIGN_7.addCurl(fp.Finger.Ring, fp.FingerCurl.FullCurl, 1.0);
-SIGN_7.addCurl(fp.Finger.Ring, fp.FingerCurl.HalfCurl, 0.9);
-
-SIGN_7.addCurl(fp.Finger.Pinky, fp.FingerCurl.FullCurl, 1.0);
-SIGN_7.addCurl(fp.Finger.Pinky, fp.FingerCurl.HalfCurl, 0.9);
-
-
-//8
-const SIGN_8 = new fp.GestureDescription('8');
-
-SIGN_8.addCurl(fp.Finger.Thumb, fp.FingerCurl.NoCurl, 1.4);
-SIGN_8.addDirection(fp.Finger.Thumb, fp.FingerDirection.HorizontalLeft, 1.0);
-SIGN_8.addDirection(fp.Finger.Thumb, fp.FingerDirection.HorizontalRight, 1.0);
-
-SIGN_8.addCurl(fp.Finger.Index, fp.FingerCurl.NoCurl, 1.0);
-SIGN_8.addDirection(fp.Finger.Index, fp.FingerDirection.VerticalUp, 1.0);
-SIGN_8.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpLeft, 1.0);
-SIGN_8.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpRight, 1.0);
-
-SIGN_8.addCurl(fp.Finger.Middle, fp.FingerCurl.NoCurl, 1.0);
-SIGN_8.addDirection(fp.Finger.Middle, fp.FingerDirection.VerticalUp, 1.0);
-SIGN_8.addDirection(fp.Finger.Middle, fp.FingerDirection.DiagonalUpLeft, 1.0);
-SIGN_8.addDirection(fp.Finger.Middle, fp.FingerDirection.DiagonalUpRight, 1.0);
-
-SIGN_8.addCurl(fp.Finger.Ring, fp.FingerCurl.FullCurl, 1.0);
-SIGN_8.addCurl(fp.Finger.Ring, fp.FingerCurl.HalfCurl, 0.9);
-
-SIGN_8.addCurl(fp.Finger.Pinky, fp.FingerCurl.FullCurl, 1.0);
-SIGN_8.addCurl(fp.Finger.Pinky, fp.FingerCurl.HalfCurl, 0.9);
-
-
-//9
-const SIGN_9 = new fp.GestureDescription('9');
-
-SIGN_9.addCurl(fp.Finger.Thumb, fp.FingerCurl.NoCurl, 1.4);
-SIGN_9.addDirection(fp.Finger.Thumb, fp.FingerDirection.HorizontalLeft, 1.0);
-SIGN_9.addDirection(fp.Finger.Thumb, fp.FingerDirection.HorizontalRight, 1.0);
-
-SIGN_9.addCurl(fp.Finger.Index, fp.FingerCurl.NoCurl, 1.0);
-SIGN_9.addDirection(fp.Finger.Index, fp.FingerDirection.VerticalUp, 1.0);
-SIGN_9.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpLeft, 1.0);
-SIGN_9.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpRight, 1.0);
-
-SIGN_9.addCurl(fp.Finger.Middle, fp.FingerCurl.NoCurl, 1.0);
-SIGN_9.addDirection(fp.Finger.Middle, fp.FingerDirection.VerticalUp, 1.0);
-SIGN_9.addDirection(fp.Finger.Middle, fp.FingerDirection.DiagonalUpLeft, 1.0);
-SIGN_9.addDirection(fp.Finger.Middle, fp.FingerDirection.DiagonalUpRight, 1.0);
-
-SIGN_9.addCurl(fp.Finger.Ring, fp.FingerCurl.NoCurl, 1.0);
-SIGN_9.addDirection(fp.Finger.Ring, fp.FingerDirection.VerticalUp, 1.0);
-SIGN_9.addDirection(fp.Finger.Ring, fp.FingerDirection.DiagonalUpLeft, 1.0);
-SIGN_9.addDirection(fp.Finger.Ring, fp.FingerDirection.DiagonalUpRight, 1.0);
-
-SIGN_9.addCurl(fp.Finger.Pinky, fp.FingerCurl.FullCurl, 1.6);
-SIGN_9.addCurl(fp.Finger.Pinky, fp.FingerCurl.HalfCurl, 1.6);
-
-
-
 //Gestures object
 let numberGestures = [
     SIGN_1,
     SIGN_2,
     SIGN_3,
     SIGN_4,
-    SIGN_5,
-    SIGN_6,
-    SIGN_7,
-    SIGN_8,
-    SIGN_9
+    SIGN_5
 ];
